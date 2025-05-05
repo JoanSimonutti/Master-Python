bafcf38707ae62e7e9d416fe98724f6c85bdae6c
Joan Simonutti <joansimonutticode@gmail.com>
2025-05-05 18:06:32 +0200

    first commit

---
+++ 02-variables-y-tipos/tipos.py
@@ -0,0 +1,17 @@
+nada = None # NoneType - Ausencia de valor - Ejemplo: None
+
+cadena = "Hola soy Joan Simonutti" # str (String) - Cadenas de texto - Ejemplo: "hola", 'mundo'
+
+entero = 99 # int (Integer) - Números enteros (positivos o negativos) - Ejemplo: 6, -16, 101
+
+flotante = 4.2 # float - Números decimales - Ejemplo: 3.14, -4.2
+
+booleano = True
+
+
+
+# Imprimir variable
+print(booleano)
+
+# Mostrar tipo de dato
+print(type(booleano))
