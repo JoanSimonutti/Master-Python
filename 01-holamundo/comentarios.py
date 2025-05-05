3768e86e072991182a66858c3f9a661eb37c103f
Joan Simonutti <joansimonutticode@gmail.com>
2025-05-05 11:43:48 +0200

    first commit

--- 01-holamundo/comentarios.py
+++ 01-holamundo/comentarios.py
@@ -8,4 +8,12 @@ print("Hola") # Esto me mostrará el texto Hola por la consola
 
 # Estas dos instrucciones muestran texto por pantalla
 print("Mundo")
-print("!!!")
+# print("!!!")
+
+"""
+Este comentario multilinea se hace con las tres comillas dobles al principio y al final.
+print("Esto no se va a mostrar")
+print("Porque está dentro de un comentario multilinea")
+"""
+
+print("Hasta luego!!!")
---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,35 @@
+"""
+Una variable es un contenedor de informacion
+que dentro guardará un dato, se pueden crear 
+muchas variables y que cada una tenga un dato distinto.
+
+"Una variable es un espacio en memoria que se utiliza para
+almacenar datos que pueden cambiar durante la ejecución del programa."
+
+Para qué sirve?
+Las variables permiten al programador guardar, modificar y utilizar datos en distintas partes del código.
+
+Características principales:
+Tiene un nombre (por ejemplo, edad, total, nombre).
+Contiene un valor (por ejemplo, 25, "Juan", 3.14).
+Puede cambiar su valor durante el programa (de ahí el nombre "variable").
+
+"""
+
+texto = "Hola! Soy Joan Simonutti y éste es mi Master en Python"
+texto2 = "Guacamole cha cha cha!"
+numero = 327
+decimal = 6.7
+
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("------------- barra separadora -------------")
+
+numero = 1022
+decimal = 10.22
+
+print(numero)
+print(decimal)
