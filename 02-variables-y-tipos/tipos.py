13b94987f59b52d7504be3efaf3ab0b445ac1693
Joan Simonutti <joansimonutticode@gmail.com>
2025-05-05 19:20:09 +0200

    first commit

--- 02-variables-y-tipos/tipos.py
+++ 02-variables-y-tipos/tipos.py
@@ -6,12 +6,18 @@ entero = 99 # int (Integer) - Números enteros (positivos o negativos) - Ejemplo
 
 flotante = 4.2 # float - Números decimales - Ejemplo: 3.14, -4.2
 
-booleano = True
+booleano = True # bool (boolean) - Verdadero o Falso - Ejemplo: True, False
+
+lista = [10, 20, 30, 100, 200] # list (Lista) - Lista ordenada, modificable - Ej: [1, 2, 3], ["a", "b", "c"]
+
+listaString = [44, "treinta", 27, "noventa"]
+
+tupla = ("Master", "en", "Python") # tuple (Tupla) - Lista ordenada, no modificable - Ej: (1, 2, 3)
 
 
 
 # Imprimir variable
-print(booleano)
+print(tupla)
 
 # Mostrar tipo de dato
-print(type(booleano))
+print(type(tupla))
